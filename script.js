984bd16734faa0de9ffc150ed0ceec5f2239c2e1
clementHunk <hunkclement@gmail.com>
2021-08-18 20:28:30 +0200

    first update

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ function errorMessage() {
     let error = document.getElementById("error")
     let email =document.getElementById("email1").value
     let mailformat =  /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/
+    var x = window.matchMedia("(max-width: 375px)")
+    if (x.matches) { // If media query matches 
     document.getElementById("button").style.marginTop= "40px" 
     document.getElementById("error-div").style.marginTop= "-70px"
+    document.getElementById("error-div").style.marginLeft= "-10px"
     document.getElementById("part2").style.marginTop="80px"
+}
     if (mailformat.test(email))
     {
         console.log("")
